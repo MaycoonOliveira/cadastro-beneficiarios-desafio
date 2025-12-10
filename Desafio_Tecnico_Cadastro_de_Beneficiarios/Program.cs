265d6200f24e08ac86f8e9b6055d91934585aa86
Maycon Oliveira <maycon009silva@gmail.com>
2025-12-10 15:15:24 -0300

    feat: Ajustando CORS para o frontend

--- Desafio_Tecnico_Cadastro_de_Beneficiarios/Program.cs
+++ Desafio_Tecnico_Cadastro_de_Beneficiarios/Program.cs
@@ -1,9 +1,9 @@
-﻿using Desafio_Tecnico_Cadastro_de_Beneficiarios.Services;
-using Desafio_Tecnico_Cadastro_de_Beneficiarios.Data;
-using Desafio_Tecnico_Cadastro_de_Beneficiarios.Profiles;
-using Desafio_Tecnico_Cadastro_de_Beneficiarios.Services.Interface;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using System.Text.Json.Serialization;
+using Desafio_Tecnico_Cadastro_de_Beneficiarios.Domain.Interface;
+using Desafio_Tecnico_Cadastro_de_Beneficiarios.Infrastructure.Data;
+using Desafio_Tecnico_Cadastro_de_Beneficiarios.Application.Services;
+using Desafio_Tecnico_Cadastro_de_Beneficiarios.Application.Profiles;
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -29,6 +29,16 @@ builder.Services.AddScoped<IBeneficiarioInterface, BeneficiarioService>();
 
 builder.Services.AddAutoMapper(typeof(PlanoProfile).Assembly);
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAngularApp", policy =>
+    {
+        policy.WithOrigins("http://localhost:4200") 
+              .AllowAnyHeader()
+              .AllowAnyMethod(); 
+    });
+});
+
 var app = builder.Build();
 
 using (var scope = app.Services.CreateScope())
@@ -43,6 +53,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseHttpsRedirection();
+app.UseCors("AllowAngularApp");
+app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
